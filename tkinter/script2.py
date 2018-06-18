98b5ee32d0ba0469f3664e0ceda321ed85c5a5c5
Ilya Semenov <semenovio@gmail.com>
2018-06-18 13:53:36 +0300

    Add files via upload

---
+++ tkinter/script2.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+window = Tk()
+
+def kg_convert():
+    #print("Something is done")
+    grams = float(e1_value.get()) * 1000
+    pounds = float(e1_value.get()) * 2.20462
+    ounces = float(e1_value.get()) * 35.274
+    t1.delete("1.0", END)
+    t1.insert(INSERT, grams)
+    t2.delete("1.0", END)
+    t2.insert(INSERT, pounds)
+    t3.delete("1.0", END)
+    t3.insert(INSERT, ounces)
+
+e0 = Label(window, text = "Kg")
+e0.grid(row = 0, column = 0)
+
+e1_value = StringVar()
+e1 = Entry(window, textvariable = e1_value)
+e1.grid(row = 0, column = 1)
+
+b1 = Button(window, text = "Convert", command = kg_convert)
+b1.grid(row = 0, column = 2)
+
+t1 = Text(window, height = 1, width = 15)
+t1.grid(row = 1, column = 0)
+
+t2 = Text(window, height = 1, width = 15)
+t2.grid(row = 1, column = 1)
+
+t3 = Text(window, height = 1, width = 15)
+t3.grid(row = 1, column = 2)
+
+window.mainloop()
