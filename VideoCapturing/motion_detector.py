86e4d3f8a31e46f90edb67d58bf5fb9ce15116aa
Ilya Semenov <semenovio@gmail.com>
2018-06-21 23:43:05 +0300

    Add files via upload

--- VideoCapturing/motion_detector.py
+++ VideoCapturing/motion_detector.py
@@ -32,6 +32,9 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
     status_list.append(status)
+
+    status_list = status_list[-2:] #drop unnecessary extra frames
+
     if len(status_list) > 1 and ((status_list[-1] == 1 and status_list[-2] == 0) or (status_list[-1] == 0 and status_list[-2] == 1)):
         times.append(datetime.now())
     cv2.imshow("Capturing", gray1)
@@ -47,8 +50,8 @@ while True:
         break
 #print(status_list)
 #print(times)
-for i in range(0, len(status_list), 2):
-    df = df.append({"Start": status_list[i], "End": status_list[i+1]}, ignore_index = True)
+for i in range(0, len(times), 2):
+    df = df.append({"Start": times[i], "End": times[i+1]}, ignore_index = True)
 df.to_csv("Times.csv")
 video.release()
 cv2.destroyAllWindows()
