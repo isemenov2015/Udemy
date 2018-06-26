084baf1adb2de1d0cde80ef6b15fcb39f0294094
Ilya Semenov <semenovio@gmail.com>
2018-06-27 02:08:31 +0300

    Add files via upload

--- Capstone_GeoCoding_Web_SQL/app.py
+++ Capstone_GeoCoding_Web_SQL/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+from werkzeug import secure_filename
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/", methods=['POST'])
+def success():
+    global data
+    if request.method == 'POST':
+        file = request.files['file']
+        fname = "uploaded_" + file.filename
+        file.save(secure_filename(fname))
+        data = pd.read_csv(fname)
+        print(data)
+    return render_template("success.html")
 
 if __name__ == '__main__':
     app.debug = True
